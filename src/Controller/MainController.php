7b22f8be5b1e409482b84d477cd72435f9ded16b
Akshay <akshayakarve@gmail.com>
2024-08-18 17:50:20 -0400

    Cleaned up code

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,14 +1,15 @@
-<?php
+<?php  
 
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class MainController
 {
     #[Route('/')]
-    public function homepage()
+    public function homepage(): Response
     {
         return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
     }
