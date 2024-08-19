458f61e0448b57646d592eabb5ec5c768e79f997
Akshay <akshayakarve@gmail.com>
2024-08-19 18:58:18 -0400

    Creating a new ships service

---
+++ src/Controller/StarshipApiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Repository\StarshipRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StarshipApiController extends AbstractController
+{
+    #[Route('/api/starships')]
+    public function getCollection(StarshipRepository $starshipRepository): Response
+    {
+        
+        $starships = $starshipRepository->findAll();
+        
+        return $this->json($starships);
+    }
+}
