b508529a7bf5a2258dd960610414f8e8beb795fc
Akshay <akshayakarve@gmail.com>
2024-08-19 20:04:16 -0400

    Added fancier routes and a detail page

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -8,14 +8,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/starships/')]
 class StarshipApiController extends AbstractController
 {
-    #[Route('/api/starships')]
+    #[Route('', methods: ['GET'])]
     public function getCollection(StarshipRepository $starshipRepository): Response
     {
         
         $starships = $starshipRepository->findAll();
         
         return $this->json($starships);
+    }  
+
+    #[Route('{id<\d+>}', methods:['GET'])]
+    public function get(int $id, StarshipRepository $repository): Response
+    {
+        $starship = $repository->find($id);
+
+        if (!$starship) {
+            throw $this->createNotFoundException("Starship not found");
+        }
+        return $this->json($starship);
     }
 }
